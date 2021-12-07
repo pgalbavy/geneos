71224bcab181ba41ff73dca7e994d32d9d893202
Peter Galbavy <peter@wonderland.org>
2021-12-07 09:58:23 +0000

    framework for log file watching, TBD

--- cmd/geneos/command_logs.go
+++ cmd/geneos/command_logs.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"path/filepath"
+
+	"github.com/fsnotify/fsnotify"
+)
+
 func init() {
 	commands["logs"] = Command{commandLogs, "logs"}
 }
@@ -9,5 +15,39 @@ func commandLogs(comp ComponentType, args []string) error {
 }
 
 func logs(c Component) (err error) {
+	logfile := filepath.Join(getString(c, Prefix(c)+"LogD"), getString(c, Prefix(c)+"LogF"))
+
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+
+	done := make(chan bool)
+	go func() {
+		for {
+			select {
+			case event := <-watcher.Events:
+				log.Println("event:", event)
+				if event.Op&fsnotify.Write == fsnotify.Write {
+					log.Println("modified file:", event.Name)
+					copyFromFile()
+				}
+			case err := <-watcher.Errors:
+				log.Println("error:", err)
+			}
+		}
+	}()
+
+	err = watcher.Add(logfile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	<-done
+
 	return ErrNotSupported
+}
+
+func copyFromFile() {
+
 }
--- go.mod
+++ go.mod
@@ -3,12 +3,16 @@ module wonderland.org/geneos
 go 1.17
 
 retract (
-    v0.3.0 // published too early
-    v0.3.1 // published too early
-    v0.3.2 // published too early
-    v0.3.3 // published too early
-    v0.3.4 // published too early
-    v0.4.0 // published too early
-    v0.4.1 // published too early
-    v0.4.2 // published too early
+	v0.4.2 // published too early
+	v0.4.1 // published too early
+	v0.4.0 // published too early
+	v0.3.4 // published too early
+	v0.3.3 // published too early
+	v0.3.2 // published too early
+	v0.3.1 // published too early
+	v0.3.0 // published too early
 )
+
+require github.com/fsnotify/fsnotify v1.5.1
+
+require golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
