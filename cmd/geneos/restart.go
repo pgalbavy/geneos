3c2d5756ae6788f0437c63bd1c065be31e1c8dc6
Peter Galbavy <peter@wonderland.org>
2021-12-23 10:20:14 +0000

    ignore non-existing process for restart

--- cmd/geneos/restart.go
+++ cmd/geneos/restart.go
@@ -1,5 +1,7 @@
 package main
 
+import "errors"
+
 func init() {
 	commands["restart"] = Command{commandRestart, nil, parseArgs, "geneos restart [TYPE] [NAME...]",
 		`Restart the matching instances. This is identical to running 'geneos stop' followed by 'geneos start'.`}
@@ -10,7 +12,7 @@ func commandRestart(ct ComponentType, args []string, params []string) (err error
 }
 
 func restartInstance(c Instance, params []string) (err error) {
-	if err = stopInstance(c, params); err == nil {
+	if err = stopInstance(c, params); err == nil || errors.Is(err, ErrProcNotExist) {
 		return startInstance(c, params)
 	}
 	return
