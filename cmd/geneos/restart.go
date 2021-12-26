76c41e9a6b9185277591741237d7a9426df9e1c7
Peter Galbavy <peter@wonderland.org>
2021-12-26 13:34:28 +0000

    add and 'all' flag

--- cmd/geneos/restart.go
+++ cmd/geneos/restart.go
@@ -1,18 +1,33 @@
 package main
 
-import "errors"
+import (
+	"errors"
+	"flag"
+)
 
 func init() {
-	commands["restart"] = Command{commandRestart, nil, parseArgs, "geneos restart [TYPE] [NAME...]",
+	commands["restart"] = Command{commandRestart, restartFlag, parseArgs, "geneos restart [TYPE] [NAME...]",
 		`Restart the matching instances. This is identical to running 'geneos stop' followed by 'geneos start'.`}
+
+	restartFlags = flag.NewFlagSet("restart", flag.ExitOnError)
+	restartFlags.BoolVar(&restartAll, "a", false, "Start all instances, not just those already running")
 }
+
+var restartFlags *flag.FlagSet
+var restartAll bool
 
 func commandRestart(ct ComponentType, args []string, params []string) (err error) {
 	return loopCommand(restartInstance, ct, args, params)
 }
 
+func restartFlag(args []string) []string {
+	restartFlags.Parse(args)
+	return restartFlags.Args()
+}
+
 func restartInstance(c Instance, params []string) (err error) {
-	if err = stopInstance(c, params); err == nil || errors.Is(err, ErrProcNotExist) {
+	err = stopInstance(c, params)
+	if err == nil || (errors.Is(err, ErrProcNotExist) && restartAll) {
 		return startInstance(c, params)
 	}
 	return
